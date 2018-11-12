1cdf0475ca75c35b50d4304705859168ed2d7cbb
NguyenTrungTruc5696 <1651012210truc@ou.edu.vn>
2018-11-12 20:12:43 +0700

    message

---
+++ bai tap ES6/bt 1.js
@@ -0,0 +1,11 @@
+let x =10;
+document.writeln("<pre>");
+document.writeln("The fist value is " + x);
+
+function test () {
+	let x = 5;
+	document.writeln("The second value is " + x);
+}
+test();
+document.writeln("The last value is " + x);
+document.write("</pre>");
